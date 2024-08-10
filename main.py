0496861139b20945c9d444dcc7440fb5da7c13c3
JDJG <ghostzap123@gmail.com>
2024-08-10 18:22:03 -0400

    Adds packages and whatnot.

---
+++ main.py
@@ -0,0 +1,26 @@
+from contextlib import asynccontextmanager
+import os
+
+from dotenv import load_dotenv
+from fastapi
+import uvicorn
+from fastapi.responses import JSONResponse, Response
+
+if not os.getenv("DB_URL"):
+    load_dotenv()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with asyncpg.create_pool(os.env["DB_URL"]) as app.pool:
+        yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+async def root():
+    return JSONResponse(content={"message": "welcome to melody api"})
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=2343, log_level="debug")
