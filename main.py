9f012bdea2c5b919b31f0796b1eb5b86330d0393
JDJG <ghostzap123@gmail.com>
2024-08-10 20:42:57 -0400

    Changes values.

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import os
 from contextlib import asynccontextmanager
 
+import asyncpg
 import fastapi
 import uvicorn
 from dotenv import load_dotenv
 from fastapi.responses import JSONResponse, Response
 
-if not os.getenv("DB_URL"):
+if not os.getenv("DB_KEY"):
     load_dotenv()
+
+class CustomRecordClass(asyncpg.Record):
+    def __getattr__(self, name: str) -> Any:
+        if name in self.keys():
+            return self[name]
+        return super().__getattr__(name)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with asyncpg.create_pool(os.env["DB_URL"]) as app.pool:
+    async with asyncpg.create_pool(os.env["DB_KEY"], record_class=CustomRecordClass) as app.pool:
         yield
 
 
